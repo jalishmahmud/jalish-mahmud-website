a39074641498055b92ac5436a740147ccdf5042f
jalish-jbc <jbc.jalish@gmail.com>
2026-08-25 16:13:23 +0600

    add node module

---
+++ .next/server/chunks/ssr/app_layout_07uf2ib.js
@@ -0,0 +1,3 @@
+module.exports=[62925,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsxs)("html",{lang:"en","data-theme":"dark",children:[(0,b.jsx)("head",{}),(0,b.jsx)("body",{children:a})]})},"metadata",0,{title:"Jalish Mahmud | Full Stack Software Engineer",description:"Portfolio of Jalish Mahmud, a full stack software engineer."}])},19325,function(a){a.n(a.i(62925))}];
+
+//# sourceMappingURL=app_layout_07uf2ib.js.map
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AezHUK5C/B2l7XToleAJCdk+M4Wa91fmbJixr1TduIY=\"\n}"
